09f11467e4c91ae54ba860ecbe12e42c59f0f4b6
Isaac <chungisaac1217@gmail.com>
2023-02-16 00:10:01 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='yaml2workflow',
-    version='0.2.2',
+    version='0.2.3',
     license='MIT',
     author="Isaac Chung",
     author_email='chungisaac1217@gmail.com',
