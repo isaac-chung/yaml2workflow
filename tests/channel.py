f461d52c7e5f9e88461499131c176434bae80203
Marcel Moldovan <marcel.moldovan@clarifai.com>
2023-03-07 14:25:47 +0200

    Fix check

--- tests/channel.py
+++ tests/channel.py
@@ -6,7 +6,7 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 def get_test_channel():
   if os.environ.get("CLARIFAI_GRPC_BASE") == "localhost":
     kwargs = {}
-    if os.environ.get("CLARIFAI_GRPC_BASE"):
+    if os.environ.get("CLARIFAI_GRPC_PORT"):
       kwargs["port"] = os.environ.get("CLARIFAI_GRPC_PORT")
     channel = ClarifaiChannel.get_insecure_grpc_channel(**kwargs)
   else:
